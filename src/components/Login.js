cbc42fc348db78a36734513f54fde1691f13d5aa
tonan <tonantruong@hotmail.com>
2023-07-09 17:28:01 -0400

    Programmatic Navigation

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,7 +20,7 @@ function Login({ setIsLoggedIn }) {
 
     setIsLoggedIn(true);
 
-    // after logging the user in, redirect to the home page!
+    // after logging the user in, redirect to the home page!!!
     history.push("/");
   }
 
